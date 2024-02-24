52b4ff014c06f84ff0bb6ae66668ab8f0cfba1b9
YourAverageDev <andrewzhuangyue@hotmail.com>
2024-02-23 22:05:11 -0500

    Updated Version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '3.4'
+VERSION = '3.5'
 DESCRIPTION = "Some external utilities to facilitate OpenAI's Utilites"
 
 with open("README.md", 'r') as r:
